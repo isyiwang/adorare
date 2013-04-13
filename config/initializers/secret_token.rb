069356cf1ad12ef9ba5d8803e3e92f646742ae3b
isyiwang <isaacwang@gmail.com>
2013-04-13 16:28:21 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adorare::Application.config.secret_token = 'de50345291ea287c592e6db3cca3d6fa2f13b09257c957f49f37fdaff3e265bd214cba63a6ef67bf001d7bf2d05b7341f33094961ae94040e3f8569644777f68'
